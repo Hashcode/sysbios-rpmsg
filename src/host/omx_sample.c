306344728c6254930dcefabb53ca8f87e96bb6dc
Subramaniam C.A <subramaniam.ca@ti.com>
2011-08-30 11:58:20 -0500

    Samples: Update host sample to work with latest rpmsg-omx
    Updated the omx_sample to be in line with the latest rpmsg-omx
    implementation. The rpmsg-omx driver parses the data packet being
    sent to perform verification and translation of any buffer handles.
    The number of buffer handles being passed in the buffer is expected
    at the beginning of the data payload in the omx packet.
    
    Signed-off-by: Anagani Chandra <chandu@ti.com>
    Signed-off-by: Subramaniam C.A <subramaniam.ca@ti.com>
    Signed-off-by: Suman Anna <s-anna@ti.com>

--- src/host/omx_sample.c
+++ src/host/omx_sample.c
@@ -32,7 +32,8 @@
 /*
  * omx_sample.c
  *
- * User space example showing how to create/connect to an OMX component server.
+ * User space example showing how to create/connect/communicate
+ * to an OMX component server.
  *
  */
 
@@ -53,61 +54,9 @@
 
 #include "omx_packet.h"
 
-#define CALLBACK_DATA      "OMX_Callback"
-
-int num_iterations = 1;
-
-int exec_cmd(int fd, char *msg, int len, char *reply_msg, int *reply_len, int cb)
-{
-    int ret = 0;
-    int cb_len;
-    char              callback_buf[512] = {0};
-    omx_packet        *cb_packet = (omx_packet *)callback_buf;
-
-    ret = write(fd, msg, len);
-    if (ret < 0) {
-         perror("Can't write to OMX instance");
-         return -1;
-    }
-
-    if (cb)  {
-        /* Get intervening callback message, sent from RPC_SKEL_GetHandle() */
-        cb_len = sizeof(omx_packet) + sizeof(CALLBACK_DATA);
-        ret = read(fd, callback_buf, cb_len);
-        if (ret < 0) {
-             perror("Did not get callback OMX message");
-             return -1;
-        }
-        else {
-            printf("\tmsg_id: %d, fxn_idx: %d, data_size: %d, data: %s\n",
-                     cb_packet->msg_id, cb_packet->fxn_idx, cb_packet->data_size,
-                     cb_packet->data);
-        }
-    }
-
-    /* Now, await normal function result from OMX service: */
-    // Note: len should be max length of response expected.
-    ret = read(fd, reply_msg, len);
-    if (ret < 0) {
-         perror("Can't read from OMX instance");
-         return -1;
-    }
-    else {
-          *reply_len = ret;
-    }
-    return(0);
-}
-
-
-void init_omx_packet(omx_packet *packet, uint16_t desc)
-{
-    /* initialize the packet structure */
-    packet->desc  |= desc << OMX_DESC_TYPE_SHIFT;
-    packet->msg_id  = 0;
-    packet->flags  = OMX_POOLID_JOBID_NONE;
-    packet->fxn_idx = OMX_INVALIDFXNIDX;
-    packet->result = 0;
-}
+#define CALLBACK_DATA       "OMX_Callback"
+#define SUCCESS             0
+#define ERROR               -1
 
 /* Some Example OMX Application stuff: */
 #define H264_DECODER_NAME   "H264_decoder"
@@ -122,17 +71,85 @@ void init_omx_packet(omx_packet *packet, uint16_t desc)
 /* OMX_HANDLETYPE defined elsewhere, but for this example ... */
 typedef uint32_t OMX_HANDLETYPE;
 
-void test_h264_decoder(int fd, int i)
+/* The map_info_type enum is defined in the rpmsg_omx driver, and is defined
+ * elsewhere in userspace, but define it here for this sample. The data portion
+ * of the packet being written is expected to start with this.
+ */
+typedef enum {
+    RPC_OMX_MAP_INFO_NONE       = 0,
+    RPC_OMX_MAP_INFO_ONE_BUF    = 1,
+    RPC_OMX_MAP_INFO_TWO_BUF    = 2,
+    RPC_OMX_MAP_INFO_THREE_BUF  = 3,
+    RPC_OMX_MAP_INFO_MAX        = 0x7FFFFFFF
+} map_info_type;
+
+int num_iterations = 1;
+
+int exec_cmd(int fd, char *msg, size_t len, char *reply_msg, int *reply_len, int cb)
+{
+    int ret = 0;
+    int cb_len;
+    char              callback_buf[512] = {0};
+    omx_packet        *cb_packet = (omx_packet *)callback_buf;
+
+    ret = write(fd, msg, len);
+    if (ret < 0) {
+        perror("Can't write to OMX instance");
+        return ERROR;
+    }
+
+    if (cb)  {
+        /* Get intervening callback message, sent from RPC_SKEL_GetHandle() */
+        cb_len = sizeof(omx_packet) + sizeof(CALLBACK_DATA);
+        ret = read(fd, callback_buf, cb_len);
+        if (ret < 0) {
+            perror("Did not get callback OMX message");
+            return ERROR;
+        }
+        else {
+            printf("\tmsg_id: %d, fxn_idx: %d, data_size: %d, data: %s\n",
+                     cb_packet->msg_id, cb_packet->fxn_idx, cb_packet->data_size,
+                     cb_packet->data);
+        }
+    }
+
+    /* Now, await normal function result from OMX service:
+     * Note: len should be max length of response expected.
+     */
+    ret = read(fd, reply_msg, len);
+    if (ret < 0) {
+        perror("Can't read from OMX instance");
+        return ERROR;
+    }
+    else {
+        *reply_len = ret;
+    }
+    return SUCCESS;
+}
+
+
+void init_omx_packet(omx_packet *packet, uint16_t desc)
+{
+    /* initialize the packet structure */
+    packet->desc  |= desc << OMX_DESC_TYPE_SHIFT;
+    packet->msg_id  = 0;
+    packet->flags  = OMX_POOLID_JOBID_NONE;
+    packet->fxn_idx = OMX_INVALIDFXNIDX;
+    packet->result = 0;
+}
+
+int test_h264_decoder(int fd, int i)
 {
     uint16_t          server_status;
-    int               packet_len;
+    size_t            packet_len;
     int               reply_len;
+    int               ret;
     char              packet_buf[512] = {0};
     char              return_buf[512] = {0};
     omx_packet        *packet = (omx_packet *)packet_buf;
     omx_packet        *rtn_packet = (omx_packet *)return_buf;
     OMX_HANDLETYPE    omx_handle;
-
+    map_info_type     map_info = RPC_OMX_MAP_INFO_NONE;
     /* Call OMX_GetHandle to create an H264_decoder: */
 
     /* Set Packet Header for the RCMServer, synchronous execution: */
@@ -141,103 +158,116 @@ void test_h264_decoder(int fd, int i)
     /* Set OMX Function Index to call, with data: */
     packet->fxn_idx = FXN_IDX_OMX_GETHANDLE;
     /* Set data for the OMX function: */
-    packet->data_size = strlen(H264_DECODER_NAME) + 1;
-    strcpy((char *)&packet->data, H264_DECODER_NAME);
+    packet->data_size = strlen(H264_DECODER_NAME) + sizeof(map_info) + 1;
+    memcpy(packet->data, &map_info, sizeof(map_info));
+    strcpy((char *)&packet->data + sizeof(map_info), H264_DECODER_NAME);
     *(char *)&packet->data[packet->data_size] = '\0';
 
     /* Exec command: */
     packet_len = sizeof(omx_packet) + packet->data_size;
     printf("omx_sample (%d): OMX_GetHandle (%s).\n", i, H264_DECODER_NAME);
-    exec_cmd(fd, (char *)packet, packet_len, (char *)rtn_packet, &reply_len, 1);
+    ret = exec_cmd(fd, (char *)packet, packet_len, (char *)rtn_packet, &reply_len, 1);
+    if (SUCCESS != ret) {
+        printf("omx_sample exec cmd failed\n");
+        return ERROR;
+    }
 
     /* Decode reply: */
     server_status = (OMX_DESC_TYPE_MASK & rtn_packet->desc) >>
             OMX_DESC_TYPE_SHIFT;
     if (server_status == OMXSERVER_STATUS_SUCCESS)  {
-       omx_handle = *(OMX_HANDLETYPE *)rtn_packet->data;
-       printf("omx_sample (%d): Got omx_handle: 0x%x\n", i, omx_handle);
+        omx_handle = *(OMX_HANDLETYPE *)rtn_packet->data;
+        printf("omx_sample (%d): Got omx_handle: 0x%x\n", i, omx_handle);
     }
     else {
-       printf("omx_sample: Failed to execute OMX_GetHandle: server status: %d\n",
+        printf("omx_sample: Failed to execute OMX_GetHandle: server status: %d\n",
             server_status);
     }
 
     /* now call SetParameter, passing back omx_handle...*/
     init_omx_packet(packet, OMX_DESC_MSG);
     packet->fxn_idx = FXN_IDX_OMX_SETPARAMETER;
-    packet->data_size = sizeof(uint32_t);
-    packet->data[0] = omx_handle;
+    packet->data_size = sizeof(OMX_HANDLETYPE) + sizeof(map_info);
+    memcpy(packet->data, &map_info, sizeof(map_info));
+    memcpy(packet->data + sizeof(map_info), &omx_handle, sizeof(OMX_HANDLETYPE));
 
     /* Exec command: */
     packet_len = sizeof(omx_packet) + packet->data_size;
     printf("omx_sample(%d): OMX_SetParameter (0x%x)\n", i, omx_handle);
-    exec_cmd(fd, (char *)packet, packet_len, (char *)rtn_packet, &reply_len, 0);
+    ret = exec_cmd(fd, (char *)packet, packet_len, (char *)rtn_packet, &reply_len, 0);
+    if (SUCCESS != ret) {
+        printf("omx_sample exec cmd failed\n");
+        return ERROR;
+    }
 
     /* Decode reply: */
     server_status = (OMX_DESC_TYPE_MASK & rtn_packet->desc) >>
             OMX_DESC_TYPE_SHIFT;
     if (server_status == OMXSERVER_STATUS_SUCCESS)  {
-       printf("omx_sample (%d): Got result %d\n", i, rtn_packet->result);
+        printf("omx_sample (%d): Got result %d\n", i, rtn_packet->result);
     }
     else {
-       printf("omx_sample: Failed to execute OMX_SetParameter: server status: %d\n",
+        printf("omx_sample: Failed to execute OMX_SetParameter: server status: %d\n",
             server_status);
     }
 
-    /* Etc... */
+    return SUCCESS;
 }
 
 
 int main(int argc, char *argv[])
 {
-       int fd;
-       int ret;
-       uint64_t die_event = 1;
-       ssize_t s;
-       struct omx_conn_req connreq = { .name = "OMX" };
-       int i;
+    int fd;
+    int ret;
+    uint64_t die_event = 1;
+    ssize_t s;
+    struct omx_conn_req connreq = { .name = "OMX" };
+    int i;
 
-       if (argc > 2)  {
-            printf("Usage: omx_sample [<num_iterations>]\n");
-            return 1;
-       }
-       else if (argc == 2) {
-            num_iterations = atoi(argv[1]);
-       }
-       else {
-            num_iterations = 1;
-       }
+    if (argc > 2)  {
+        printf("Usage: omx_sample [<num_iterations>]\n");
+        return ERROR;
+    }
+    else if (argc == 2) {
+        num_iterations = atoi(argv[1]);
+    }
+    else {
+        num_iterations = 1;
+    }
 
-       /* Connect to the OMX ServiceMgr on Ducati core 1: */
-       fd = open("/dev/rpmsg-omx1", O_RDWR);
-       if (fd < 0) {
-            perror("Can't open OMX device");
-            return 1;
-       }
+    /* Connect to the OMX ServiceMgr on Ducati core 1: */
+    fd = open("/dev/rpmsg-omx1", O_RDWR);
+    if (fd < 0) {
+        perror("Can't open OMX device");
+        return ERROR;
+    }
 
-       /* Create an OMX server instance, and rebind its address to this
-        * file descriptor.
-        */
-       ret = ioctl(fd, OMX_IOCCONNECT, &connreq);
-       if (ret < 0) {
-            perror("Can't connect to OMX instance");
-            return 1;
-       }
+    /* Create an OMX server instance, and rebind its address to this
+     * file descriptor.
+     */
+    ret = ioctl(fd, OMX_IOCCONNECT, &connreq);
+    if (ret < 0) {
+        perror("Can't connect to OMX instance");
+        return ERROR;
+    }
+    printf("omx_sample: Connected to %s\n", connreq.name);
 
-       printf("omx_sample: Connected to %s\n", connreq.name);
+    for (i = 1; i <= num_iterations; i++) {
+        ret = test_h264_decoder(fd, i);
+        if (SUCCESS != ret) {
+            printf("dummy decoder test failed\n");
+            return ERROR;
+        }
+    }
 
-       for (i = 1; i <= num_iterations; i++ ) {
-            test_h264_decoder(fd, i);
-       }
+    /* Terminate connection and destroy OMX instance */
+    ret = close(fd);
+    if (ret < 0) {
+        perror("Can't close OMX fd ??");
+        return ERROR;
+    }
 
-       /* Terminate connection and destroy OMX instance */
-       ret = close(fd);
-       if (ret < 0) {
-            perror("Can't close OMX fd ??");
-            return 1;
-       }
+    printf("omx_sample: Closed connection to %s!\n", connreq.name);
 
-       printf("omx_sample: Closed connection to %s!\n", connreq.name);
-
-       return 0;
+    return SUCCESS;
 }
